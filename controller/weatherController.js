ffab24a4bc8a3ea8a02d8b656ca6ae00c726255b
Pathak <PATHAKABHISHEK0003@GMAIL.COM>
2020-04-13 19:38:23 +0530

    create REST Api for getting curent weather data based on latitude and longitude

--- controller/weatherController.js
+++ controller/weatherController.js
@@ -1,6 +1,48 @@
+require("dotenv").config();
+
+const request = require("request");
+
 class WeatherController {
   constructor(weatherRouter) {
     this.weatherRouter = weatherRouter;
+    this.registerRoutes();
+  }
+
+  // This method register routes of WeatherController
+  registerRoutes() {
+    this.weatherRouter.get(
+      "/current_weather_data",
+      this.getCurrentWeatherData.bind(this)
+    );
+  }
+
+  // This api gets current weather data based on latitude and longitude paramters passed
+  getCurrentWeatherData(req, res, next) {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let responseObject = {
+      weatherData: null,
+      errorMessage: null,
+    };
+    if (lat && lon) {
+      request(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`,
+        function (error, response, body) {
+          if (error) {
+            responseObject.errorMessage =
+              "Sorry for Inconvenience, Unable to Fetch Data.";
+            res.send(responseObject);
+          } else if (response.statusCode === 200) {
+            responseObject.weatherData = JSON.parse(body);
+            res.send(responseObject);
+          }
+        }
+      );
+    } else {
+      responseObject.errorMessage =
+        "Provide Both lat and lon query paramaters.";
+      res.send(responseObject);
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+require("dotenv").config();
 /** 
  CONFIGURATIONS
 */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const express = require("express");
+const weatherController = require("./controller/weatherController")
+  .weatherController;
+
 const app = express();
 
 const weatherRouter = express.Router();
 app.use("/api/v1/weather", weatherRouter);
+
+/**
+ * passed weatherRouter in weatherController so that sub routes
+ * can be registered in weatherController
+ */
+weatherController(weatherRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
