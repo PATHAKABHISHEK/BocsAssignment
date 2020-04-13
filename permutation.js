490fbe9d6a13147366310a3e8adc2f182c903775
Pathak <PATHAKABHISHEK0003@GMAIL.COM>
2020-04-13 20:04:44 +0530

    added code for finding out all permutations of a string

---
+++ permutation.js
@@ -0,0 +1,32 @@
+const readline = require("readline");
+
+/**
+ * Finding all permutations of particular string
+ */
+const findAllPermutationsOfString = (someString) => {
+  if (someString.length < 2) {
+    return someString;
+  }
+
+  let permutationsArray = [];
+  for (let i = 0; i < someString.length; i++) {
+    let singleCharachter = someString[i];
+
+    if (someString.indexOf(singleCharachter) != i) continue;
+
+    let remainingChars =
+      someString.slice(0, i) + someString.slice(i + 1, someString.length);
+    for (let permutation of findAllPermutationsOfString(remainingChars)) {
+      permutationsArray.push(singleCharachter + permutation);
+    }
+  }
+  return permutationsArray;
+};
+
+// Change the Value of someString to see Permutation in action
+// and then run this file using node permutation.js
+const someString = "abcd";
+
+findAllPermutationsOfString(someString).forEach((permutatedString) => {
+  console.log(permutatedString);
+});
